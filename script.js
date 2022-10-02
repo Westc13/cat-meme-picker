7c977904b105acf26eb500a1a38303bda4c96a21
ChristineWest <christinewwwest@gmail.com>
2022-10-02 13:52:50 -0400

    created a function to render all emotions from catsData

--- script.js
+++ script.js
@@ -208,4 +208,21 @@ function getEmotionsArray(cats) {
      }*/
 }
 
-getEmotionsArray(catsData);
+function renderEmotionsRadios(cats) {
+  //TODO: Challenge
+  /*
+    1. Have the new function "renderEmotionsRadios" 
+       take in a single parameter. Name that parameter
+       "cats". 
+    2. Inside renderEmotionsRadios, set up a const called 
+       "emotions" and set it equals to whatever is returned 
+       by calling getEmotionsArray, passing in "cats" as an 
+       argument.
+    3. For now, renderEmotionsRadios should just log out 
+       "emotions".
+    4. Call renderEmotionsRadios passing in catsData.
+    */
+  const emotions = getEmotionsArray(cats);
+  console.log(emotions);
+}
+renderEmotionsRadios(catsData);
